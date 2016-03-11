ae6a587517bbbb6a9b6b20848a0d4337f9ce2f55
Augusto Ramirez <augedsa@gmail.com>
2016-03-11 17:19:22 -0430

    Uploading files

---
+++ js/handler.js
@@ -0,0 +1,4 @@
+//Brain Handler.
+
+init();
+animate();
---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    //$("#").click(function() {
+    // });
+
+    // $("#, .").click(function() {
+    // });
+
+});
